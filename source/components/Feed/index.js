060ee4aed902c308855a6f2061cecd785dbc4172
mosterbox <romapotapov7@gmail.com>
2019-01-21 16:30:24 +0200

    add composer and provider, refactoring props

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -15,12 +15,9 @@ export default class Feed extends Component {
 
         return (
             <section className = { Styles.feed }>
-                <StatusBar { ...this.props }/>
-                <Composer
-                    avatar = { avatar }
-                    currentUserFirstName = { currentUserFirstName }
-                />
-                <Post { ...this.props }/>
+                <StatusBar />
+                <Composer />
+                <Post />
             </section>
         );
     }
